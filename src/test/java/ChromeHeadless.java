6d995c3624c5281073ef972b07d61f2af97c0a93
Carlos <carloseduleal@gmail.com>
2017-10-06 21:29:35 +0000

    aa

--- src/test/java/ChromeHeadless.java
+++ src/test/java/ChromeHeadless.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
+import static org.junit.Assert.assertEquals;
 import java.io.File;
 
 public class ChromeHeadless {
@@ -14,19 +14,20 @@ public class ChromeHeadless {
 
     @Test
     public void openBrowserAndDoSomething(){
-        //System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+//        System.setProperty("webdriver.chrome.driver","drivers/chromedriver_linux");
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
 
-        ChromeDriverService service = new ChromeDriverService.Builder()
-                .withEnvironment(ImmutableMap.of("DISPLAY",":99"))
-                .build();
+//        ChromeDriverService service = new ChromeDriverService.Builder()
+  //              .withEnvironment(ImmutableMap.of("DISPLAY",":99"))
+    //            .build();
 
-        driver = new ChromeDriver(service, options);
+        driver = new ChromeDriver(options);
 
         driver.get("http://www.google.com");
-
+	assertEquals(driver.getTitle(),"Google");
+       
         driver.quit();
     }
 
