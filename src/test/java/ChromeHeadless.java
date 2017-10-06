cd8f90274fb82ca2c72b908e45ac39cd1eb9b588
Carlos Leal <celeal@thoughtworks.com>
2017-10-06 14:29:04 -0300

    agora vai

--- src/test/java/ChromeHeadless.java
+++ src/test/java/ChromeHeadless.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
@@ -15,13 +16,17 @@ public class ChromeHeadless {
     public void openBrowserAndDoSomething(){
         //System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
 
-        ChromeDriverService service = new ChromeDriverService.Builder()
-                .usingDriverExecutable(new File("/usr/bin/chromedriver"))
-                .usingAnyFreePort()
-                .withEnvironment(ImmutableMap.of("DISPLAY",":99"))
-                .build();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
 
-        driver = new ChromeDriver(service);
+//        ChromeDriverService service = new ChromeDriverService.Builder()
+//                .usingDriverExecutable(new File("/usr/bin/chromium-browser"))
+//                .usingAnyFreePort()
+//                .withEnvironment(ImmutableMap.of("DISPLAY",":99"))
+//                .build();
+
+        driver = new ChromeDriver(options);
+
         driver.get("http://www.google.com");
 
         driver.quit();
